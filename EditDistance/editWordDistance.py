cd9e80f758b9acb35a9118001e5fe26bca0be495
mburman <mburman@andrew.cmu.edu>
2013-02-23 17:12:08 -0500

    Formatting fixes.

--- EditDistance/editWordDistance.py
+++ EditDistance/editWordDistance.py
@@ -1,69 +1,53 @@
 #!/usr/local/bin/python
 
 import sys,string
-def levenshteinDistance(word1, word2, memo):
-	len1 = len(word1)
-	len2 = len(word2)
 
-	if (word1 == ""): return len2
-	if (word2 == ""): return len1
-
-	key = str([word1, word2])
-	if key in memo:
-		return memo[key]
-
-	word1MinusOne = word1[:(len1-1)]
-	word2MinusOne = word2[:(len2-1)]
-	if (word1[len1-1] == word2[len2-1]):
-		count = levenshteinDistance(word1MinusOne, word2MinusOne, memo)
-	else:
-		count1 = levenshteinDistance(word1MinusOne,word2, memo) + 1
-		count2 = levenshteinDistance(word1, word2MinusOne, memo) + 1
-		count3 = levenshteinDistance(word1MinusOne, word2MinusOne, memo) + 1
-		count = min(count1, count2, count3)
-	memo[key] = count
-	return count
+INSERTION_COST = 1
+DELETION_COST = 1
+REPLACEMENT_COST = 1
 
 def splitSentence(sentence):
-        s = sentence.split()
-        for i in range(len(s)):
-                s[i] = s[i].strip()
-        return s
+  s = sentence.split()
+  for i in range(len(s)):
+    s[i] = s[i].strip()
+  return s
 
 def damerauDistance(s1, s2, memo):
-        len1 = len(s1)
-        len2 = len(s2)
+  len1 = len(s1)
+  len2 = len(s2)
 
-        if (len1 == 0): return len2
-        if (len2 == 0): return len1
+  if (len1 == 0): return len2
+  if (len2 == 0): return len1
 
-        key = str([s1, s2])
-        if key in memo:
-                return memo[key]
-        sentence1MinusOne = s1[:(len1-1)]
-        sentence2MinusOne = s2[:(len2-1)]
-        if (s1[len1-1] == s2[len2-1]):
-                count = damerauDistance(sentence1MinusOne, sentence2MinusOne, memo)
-        else:
-                count4 = float("infinity")
-                if ((len1 > 1) and (len2 > 1)):
-                        lastWordSentence1 = s1[len1-1]
-                        secondLastWordSentence1 = s1[len1-2]
-                        lastWordSentence2 = s2[len2-1]
-                        secondLastWordSentence2 = s2[len2-2]
-                        # Swapped letters
-                        if ((lastWordSentence1 == secondLastWordSentence2) and
-                            (lastWordSentence2 == secondLastWordSentence1)):
-                                count4 = damerauDistance(s1[:len1-2],
-                                                             s2[:len2-2],
-                                                         memo) + 1
+  key = str([s1, s2])
+  if key in memo:
+          return memo[key]
+  sentence1MinusOne = s1[:(len1-1)]
+  sentence2MinusOne = s2[:(len2-1)]
+  if (s1[len1-1] == s2[len2-1]):
+    count = damerauDistance(sentence1MinusOne, sentence2MinusOne, memo)
+  else:
+    count4 = float("infinity")
+    if ((len1 > 1) and (len2 > 1)):
+       lastWordSentence1 = s1[len1-1]
+       secondLastWordSentence1 = s1[len1-2]
+       lastWordSentence2 = s2[len2-1]
+       secondLastWordSentence2 = s2[len2-2]
 
-                count1 = damerauDistance(sentence1MinusOne,s2, memo) + 1
-                count2 = damerauDistance(s1, sentence2MinusOne, memo) + 1
-                count3 = damerauDistance(sentence1MinusOne, sentence2MinusOne, memo) + 1
-                count = min(count1, count2, count3, count4)
-        memo[key] = count
-        return count
+       # Word transposition.
+       if ((lastWordSentence1 == secondLastWordSentence2) and
+           (lastWordSentence2 == secondLastWordSentence1)):
+         count4 = damerauDistance(s1[:len1-2], s2[:len2-2], memo) + \
+             TRANSPOSITION_COST
+
+    count1 = damerauDistance(sentence1MinusOne,s2, memo) + DELETION_COST
+    count2 = damerauDistance(s1, sentence2MinusOne, memo) + INSERTION_COST
+    count3 = damerauDistance(sentence1MinusOne, sentence2MinusOne, memo) + \
+        REPLACEMENT_COST
+
+    count = min(count1, count2, count3, count4)
+  memo[key] = count
+  return count
 
 def makeSentenceArray(inputFile):
 	f = open(inputFile, "r")
@@ -75,32 +59,26 @@ def makeSentenceArray(inputFile):
 
 
 def computeDistances(questionFile, answerFile):
-    answerArray = makeSentenceArray(answerFile)
-    questionArray = makeSentenceArray(questionFile)
-    # output = open(outputFile, "w")
-    for question in questionArray:
-        closestAnswer = ""
-        sentenceDistance = float("infinity")
-        memo = dict()
-        for answer in answerArray:
-          exclude = set(string.punctuation)
-          answer_edited = ''.join(ch for ch in answer.lower() if ch not in exclude)
-          question_edited = ''.join(ch for ch in question.lower() if ch not in exclude)
+  answerArray = makeSentenceArray(answerFile)
+  questionArray = makeSentenceArray(questionFile)
+  # output = open(outputFile, "w")
+  for question in questionArray:
+      closestAnswer = ""
+      sentenceDistance = float("infinity")
+      memo = dict()
+      for answer in answerArray:
+        exclude = set(string.punctuation)
+        answer_edited = ''.join(ch for ch in answer.lower() if ch not in exclude)
+        question_edited = ''.join(ch for ch in question.lower() if ch not in exclude)
 
-          count = damerauDistance(splitSentence(question_edited), splitSentence(answer_edited), memo)
+        count = damerauDistance(splitSentence(question_edited), splitSentence(answer_edited), memo)
 
-          if (count < sentenceDistance):
-                closestSentence = answer
-                sentenceDistance = count
-        print "Question sentence: " + question
-        print "Answer sentence: " + closestSentence
-        print "Damerau distance: " + str(sentenceDistance) + "\n"
-
-        # output.write("Question sentence: " + question + "\n")
-        # output.write("Answer sentence: " + closestSentence + "\n")
-        # output.write("Damerau distance: " + str(sentenceDistance) + "\n\n")
-    # output.close()
-
+        if (count < sentenceDistance):
+              closestSentence = answer
+              sentenceDistance = count
+      print "Question sentence: " + question
+      print "Answer sentence: " + closestSentence
+      print "Damerau distance: " + str(sentenceDistance) + "\n"
 
 
 if __name__ == '__main__':
