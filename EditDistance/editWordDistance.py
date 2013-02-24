6049a187683e11b38caa402af5d91f39753c74f9
mburman <mburman@andrew.cmu.edu>
2013-02-23 19:34:56 -0500

    Words that are in the beginning of a question (like who, what, where etc) do not help in finding the edit distance. Filtering them gives us better results.

--- EditDistance/editWordDistance.py
+++ EditDistance/editWordDistance.py
@@ -12,6 +12,31 @@ def splitSentence(sentence):
     s[i] = s[i].strip()
   return s
 
+def cleanQuestion(question):
+  exclude = set(string.punctuation)
+  question = ''.join(ch for ch in question.lower() if ch not in exclude)
+
+  #TODO(mburman): there's probably a better way to do this
+  # It's important to maintain ordering here - multi word phrases should come
+  # earlier in the list so that we can replace maximally.
+  bad_start_phrase = [
+      'how many', 'when did', 'why did',
+      'what', 'where', 'why', 'when', 'who', 'how', 'did', 'name']
+
+  # If a bad phrase is present in the beginning of a question, replace it.
+  for bad_phrase in bad_start_phrase:
+    bad_index = question.find(bad_phrase)
+    if bad_index == 0:
+      question = question.replace(bad_phrase, '', 1)
+      break
+
+  return question
+
+def cleanAnswer(answer):
+  exclude = set(string.punctuation)
+  answer = ''.join(ch for ch in answer.lower() if ch not in exclude)
+  return answer
+
 def damerauDistance(s1, s2, memo):
   len1 = len(s1)
   len2 = len(s2)
@@ -67,9 +92,8 @@ def computeDistances(questionFile, answerFile):
       sentenceDistance = float("infinity")
       memo = dict()
       for answer in answerArray:
-        exclude = set(string.punctuation)
-        answer_edited = ''.join(ch for ch in answer.lower() if ch not in exclude)
-        question_edited = ''.join(ch for ch in question.lower() if ch not in exclude)
+        answer_edited = cleanAnswer(answer)
+        question_edited = cleanQuestion(question)
 
         count = damerauDistance(splitSentence(question_edited), splitSentence(answer_edited), memo)
 
