b8a8e824dfc6f124fc322ce01735961137f981fe
mburman <mburman@andrew.cmu.edu>
2013-03-11 23:21:33 -0700

    Dependency check before running script.

--- question_generator/question_generator.py
+++ question_generator/question_generator.py
@@ -31,7 +31,7 @@ def appendToPreviousWord(word):
 def makeWhoQuestion(words):
   question_parts = ['Who']
   i = 0
-  # Ignore the name 
+  # Ignore the name
   while hasNameEntityTag(words[i], PERSON_TAG):
     i+=1
   for j in range(i,len(words)):
@@ -41,7 +41,7 @@ def makeWhoQuestion(words):
       question_parts[len(question_parts)-1] = prev_word + word
     else:
       question_parts.append(word)
-  
+
   # Last part of sentence is ending punctuation like a period.
   question_parts = question_parts[:len(question_parts)-1]
   last_word = question_parts[len(question_parts)-1]
@@ -61,6 +61,14 @@ def makeWhoQuestions(sentences):
       who_questions.append(question)
   return who_questions
 
+# Check dependencies
+def check_dependencies():
+  if os.path.isdir("../stanford-ner-2012-11-11"):
+    return
+  else:
+    print "Dependencies not installed.\nRun ./build_dependencies.sh"
+    sys.exit(0)
+
 # TODO(mburman): use the logging module instead of prints
 # TODO(mburman): let user specify logging level
 if __name__ == '__main__':
@@ -68,9 +76,9 @@ if __name__ == '__main__':
     print 'Usage: ' + \
         'python question_generator.py <file>\n'
     sys.exit(0)
-  
-  file_name = sys.argv[1]
+  check_dependencies()
 
+  file_name = sys.argv[1]
   file_path = '../question_generator/' + file_name
 
   tagged_file = open('tagged_' + file_name, 'w+')
@@ -78,11 +86,11 @@ if __name__ == '__main__':
   subprocess.call(['java', '-cp', '../stanford-ner-2012-11-11/stanford-ner.jar', '-mx600m',
                   'edu.stanford.nlp.ie.crf.CRFClassifier', '-loadClassifier',
                   '../stanford-ner-2012-11-11/classifiers/english.all.3class.distsim.crf.ser.gz', '-textFile', file_path], stdout=tagged_file)
- 
+
   tagged_file.seek(0)
   sentences = tagged_file.readlines()
   tagged_file.close()
-  
+
   questions = makeWhoQuestions(sentences)
   # Write questions to a file
   question_file = open('questions_' + file_name, 'w')
@@ -93,6 +101,6 @@ if __name__ == '__main__':
 # 's at end of name entity is counted as separate word
 # In the Dempsay article there is a typo. It says Dempsay 3rd goal was scored...
 # so our question generator makes the sentence Who 3rd goal was scored
-# The name entity recognizer marks commas and similar things as separate words 
+# The name entity recognizer marks commas and similar things as separate words
 # so we need to fix that when we recombine sentences
 # Need to deal with sentences like Bob was born here and he did this. He should be converted.
