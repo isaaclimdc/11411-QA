8d99637dee676b4ecc464c87b36b83929a42e68e
Isaac Lim <isaaclimdc@gmail.com>
2013-03-15 21:43:04 -0400

    Progress on generating "when" questions
    So today I worked on generating "when" questions on top of the existing
    "who" questions. It sorta works, except the resulting grammar is not
    that great just yet: but this certainly can be improved on. I basically
    did this by picking out all occurrences of months in the text.

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -1,3 +1,4 @@
 wget http://nlp.stanford.edu/software/stanford-ner-2012-11-11.zip
 unzip stanford-ner-2012-11-11.zip
+mv stanford-ner-2012-11-11 libraries/stanford-ner-2012-11-11
 rm stanford-ner-2012-11-11.zip
--- question_generator/question_generator.py
+++ question_generator/question_generator.py
@@ -1,12 +1,14 @@
 #!/usr/local/bin/python
 
 import logging, os, sys, string, re, subprocess, ntpath
-lib_path = os.path.abspath('../helpers')
+lib_path = os.path.abspath('../libraries')
 sys.path.append(lib_path)
+import en
 from nltk_helper import splitIntoSentences2, getSynonyms
 
 PERSON_TAG = "/PERSON"
 LOCATION_TAG = "/LOCATION"
+
 # Check to see if a word has a given tag
 # (i.e person, place)
 # TODO(sjoyner): We'll probably be able to use this function
@@ -65,7 +67,7 @@ def makeWhoQuestions(sentences):
     end = len(words)
     for i in xrange(0, len(words)):
       firstChar = words[i][0]
-      if firstChar == ',' or firstChar == ";":
+      if firstChar == ',' or firstChar == ';':
         end = i
         break
     words = words[:end]
@@ -81,6 +83,83 @@ def makeWhoQuestions(sentences):
 
   return who_questions
 
+# Does the dirty work to transform a raw sentence containing
+# a date reference to a "when" question.
+def processWhenQuestion(question_parts):
+  # Remove extra words/symbols, RECURSIVELY :(
+  def recClean(parts):
+    firstWord = parts[0]
+    lastWord = parts[-1]
+    if firstWord == "on" or firstWord == "." or \
+       firstWord == "," or firstWord.isdigit():
+      return recClean(parts[1:])
+    if lastWord == "on" or lastWord == "." or \
+       lastWord == "," or lastWord.isdigit():
+      return recClean(parts[:-1])
+
+    return parts
+
+  question_parts = recClean(question_parts)
+
+  # Convert the subject verb to present tense using NodeBox
+  # TODO(idl):A smarter version of this. This only takes
+  # care of 1 case.
+  try:
+    question_parts[1] = en.verb.present(question_parts[1])
+  except:
+    pass
+
+  # Truncate sentence after first occurence of "," or ";".
+  # This is usually enough for a question.
+  end = len(question_parts)
+  for i in xrange(0, len(question_parts)):
+    firstChar = question_parts[i][0]
+    if firstChar == ',' or firstChar == ';':
+      end = i
+      break
+  question_parts = question_parts[:end]
+
+  # Join everything together.
+  question_parts = ["When", "did"] + question_parts
+  question = " ".join(question_parts) + "?"
+  question = cleanQuestion(question)
+
+  return question
+
+# Find sentences that we can turn into
+# simple 'when' questions.
+def makeWhenQuestions(sentences):
+  # Pick out occurences of dates in the text
+  r = re.compile('january|february|march|april|may|june|july|august|september|october|november|December.*', re.IGNORECASE)
+
+  when_questions = []
+  for sentence in sentences:
+    sentence = sentence.strip()
+    words = sentence.split()
+    n = len(words)
+
+    # Strip NER tags; don't need them for when questions
+    for i in xrange(0, n):
+      words[i] = removeTag(words[i])
+
+    # Pick out the date occurences
+    for i in xrange(0, n):
+      word = words[i]
+      if r.match(word):
+        if i > n/2:
+          # Extract first half of sentence
+          extracted = words[:i]
+        else:
+          # Extract second half of sentence
+          extracted = words[i+1:]
+
+        question = processWhenQuestion(extracted)        
+        when_questions.append(question)
+        # print question
+
+  return when_questions
+
+
 # Final pass over a question to remove unnecessary tags.
 def cleanQuestion(question):
   # TODO(mburman): a single pass replace might be more efficient.
@@ -91,7 +170,7 @@ def cleanQuestion(question):
 
 # Check dependencies
 def checkDependencies():
-  if os.path.isdir("../stanford-ner-2012-11-11"):
+  if os.path.isdir("../libraries/stanford-ner-2012-11-11"):
     return
   else:
     print "Dependencies not installed.\nRun ./build_dependencies.sh"
@@ -114,9 +193,9 @@ if __name__ == '__main__':
     os.makedirs('tagged')
   tagged_file = open('tagged/tagged_' + ntpath.basename(file_name), 'w+')
   # Executes Stanford name entity recognizer.
-  subprocess.call(['java', '-cp', '../stanford-ner-2012-11-11/stanford-ner.jar', '-mx600m',
+  subprocess.call(['java', '-cp', '../libraries/stanford-ner-2012-11-11/stanford-ner.jar', '-mx600m',
                   'edu.stanford.nlp.ie.crf.CRFClassifier', '-loadClassifier',
-                  '../stanford-ner-2012-11-11/classifiers/english.all.3class.distsim.crf.ser.gz', '-textFile', file_path], stdout=tagged_file)
+                  '../libraries/stanford-ner-2012-11-11/classifiers/english.all.3class.distsim.crf.ser.gz', '-textFile', file_path], stdout=tagged_file)
   tagged_file.seek(0)
   sentences = tagged_file.readlines()
   tagged_file.close()
@@ -125,6 +204,7 @@ if __name__ == '__main__':
   file_path = '../question_generator/' + file_name
 
   questions = makeWhoQuestions(sentences)
+  questions += makeWhenQuestions(sentences)
 
   # Write questions to a file.
   if not os.path.exists('questions'):
