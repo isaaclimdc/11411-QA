9c16ca579d14239031dfe3f94b281b8917419047
Isaac Lim <isaaclimdc@gmail.com>
2013-03-20 22:43:26 -0400

    Fixed typos in parse_text and build_dependencies

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -3,12 +3,16 @@
 # mv stanford-ner-2012-11-11 libraries/stanford-ner
 # rm stanford-ner-2012-11-11.zip
 
+echo '~ Installing Stanford-CoreNLP...'
 curl -O http://nlp.stanford.edu/software/stanford-corenlp-full-2012-11-12.zip
 unzip stanford-corenlp-full-2012-11-12.zip
-mv stanford-corenlp-full-2012-11-12 libraries/stanford-cornlp
+mv stanford-corenlp-full-2012-11-12 libraries/stanford-corenlp
 rm stanford-corenlp-full-2012-11-12.zip 
+echo '~ DONE!\n'
 
+echo '~ Installing NodeBox Linguistics'
 curl -O http://nodebox.net/code/data/media/linguistics.zip
 unzip linguistics.zip
 mv en libraries/en
 rm linguistics.zip
+echo '~ DONE!\n'
--- helpers/parse_text.sh
+++ helpers/parse_text.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo 'cd before'
-cd ../libraries/stanford-corenlp-full-2012-11-12/
+cd ../libraries/stanford-corenlp/
 echo 'java'
 java -cp stanford-corenlp-1.3.4.jar:stanford-corenlp-1.3.4-models.jar:xom.jar:joda-time.jar:jollyday.jar -Xmx3g edu.stanford.nlp.pipeline.StanfordCoreNLP -annotators tokenize,ssplit,pos,lemma,ner -file $1 -outputDirectory ../../question_generator
 echo 'here'
