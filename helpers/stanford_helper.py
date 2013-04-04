1c1681e7fc9402699fc66f2eab18501809e78dc2
Shannon Joyner <sjoyner@andrew.cmu.edu>
2013-04-04 17:01:21 -0400

    dependencies

--- helpers/stanford_helper.py
+++ helpers/stanford_helper.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
 
-import string, sys, subprocess, ntpath
+import string, sys, subprocess
 
 POSITION = 'POS'
 NAMED_ENTITY = 'NER'
@@ -38,75 +38,86 @@ def parseWord(xml_file, tag_types):
       break
     line = xml_file.readline()
   tagged_word = word + tag
-  return tagged_word
+  return (word, tagged_word)
 
-def parseSentence(xml_file, tag_types):
+def parseSentence(xml_file, dependencies, tag_types):
   line = xml_file.readline()
   words = []
+  tagged_words = []
   while line != '':
     if '<token id' in line:
-      word = parseWord(xml_file, tag_types)
+      (word, tagged_word) = parseWord(xml_file, tag_types)
       words.append(word)
+      tagged_words.append(tagged_word)
+    elif '</tokens>' in line:
+      tagged_sentence = ' '.join(tagged_words)
+      sentence = ' '.join(words)
+    elif '<collapsed-dependencies>' in line:
+      print 'collapsed'
+      getDependencies(xml_file, dependencies, sentence)
     elif '</sentence>' in line:
       break
     line = xml_file.readline()
-  tagged_sentence = ' '.join(words)
-  return tagged_sentence
+  return (sentence, tagged_sentence)
+
+def parseLine(line):
+  start_index = line.find('>') + 1
+  end_index = line.find('<', start_index)
+  return line[start_index : end_index]
+
+def getDependencies(xml_file, dependencies, sentence):
+  line = xml_file.readline()
+  dep = []
+  while line != '':
+    if '<governor ' in line:
+      governor = parseLine(line)
+    elif '<dependent ' in line:
+      dependent = parseLine(line)
+      depPair = (governor, dependent)
+      dep.append(depPair)
+    elif '</collapsed-dependencies>' in line:
+      dependencies[sentence] = str(dep)
+      break
+    line = xml_file.readline()
 
 def parseXMLFile(xml_file, tag_types):
   line = xml_file.readline()
   sentences = []
+  dependencies = dict()
+  print 'before'
   while line != '':
     if '<sentence id' in line:
-      sentence = parseSentence(xml_file, tag_types)
-      sentences.append(sentence)
+      (sentence, tagged_sentence) = parseSentence(xml_file, dependencies, tag_types)
+      sentences.append(tagged_sentence)
+      tagged_sentences = '\n'.join(sentences)
     line = xml_file.readline()
-  tagged_sentences = '\n'.join(sentences)
+
+  print 'dependencies'
+  print dependencies
   return tagged_sentences
 
-def getFileName(file_path, trunc_ext):
-  file_path = ntpath.basename(file_path)
-  
-  if trunc_ext:
-    return file_path[:-4]
-  else:
-    return file_path
-
-def getXMLFilePath(file_path):
-  file_name = getFileName(file_path, False)
-  xml_path = '../helpers/tmp/' + file_name + '.xml'
-  return xml_path
-
-def getOutputFilePath(output_dir, input_path):
-  file_name = getFileName(input_path, True)
-  output_path = output_dir + file_name + '.tag'
-  return output_path
+def getXMLFileLocation(file_name):
+  xml_file = 'tmp/' + file_name + '.xml'
+  return xml_file
 
 if __name__ == '__main__':
   if len(sys.argv) < 4:
     print 'Usage: ' + \
-        './stanford_helper.py <input file> <output dir> <tag type>\n'
+        './stanford_helper.py <file> <output> <tag_type>\n'
     sys.exit(0)
 
-  input_path = sys.argv[1]
-  input_path_rel = '../../test_data/' + input_path
-  output_dir = sys.argv[2]
+  file_name = sys.argv[1]
+  file_path_stanford = '../../helpers/' + file_name
+  output_name = sys.argv[2]
   tag_types = sys.argv[3:]
-  
-  # Execute the shell script to run StanfordCoreNLP, and wait for
-  # it to complete.
-  subprocess.check_call(['./parse_text.sh', input_path_rel])
 
-  # Generate a temp xml file. For the input X.txt, generate X.txt.xml
-  xml_file = getXMLFilePath(input_path)
+  subprocess.Popen(['./parse_text.sh', file_path_stanford]).wait()
+
+  xml_file = getXMLFileLocation(file_name)
+
   xml_file = open(xml_file, 'r')
   tagged_sentences = parseXMLFile(xml_file, tag_types)
-
-  # For the input file X.txt, generate X.tags
-  output_path = getOutputFilePath(output_dir, input_path)
-  output_file = open(output_path, 'w+')
+  
+  output_file = open(output_name, 'w+')
   output_file.write(tagged_sentences)
   output_file.close()
-
-  # Clean up
-  subprocess.check_call(['./cleanup.sh'])
